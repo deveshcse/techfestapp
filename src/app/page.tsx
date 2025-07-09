12ccc67ea36dd9b3ebaf27c46e66eb6d16cbc837
deveshcse <d.mishra56603@gmail.com>
2025-07-09 23:10:40 +0530

    chore: setup and add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import LoginForm from "@/components/authentication/LoginForm";
+import SignUpForm from "@/components/authentication/SignUpForm";
+
 export default function Home() {
   return (
-    <div className="bg-red-500 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      this is a test page
+    <div className="bg-green-500 grid  items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <LoginForm/>
+    <SignUpForm/>
     </div>
   );
 }
