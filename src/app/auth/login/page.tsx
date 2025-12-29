2ace2f654eae3a5bc6b7abe86c8a99b3430e276f
deveshcse <d.mishra56603@gmail.com>
2025-12-30 02:52:43 +0530

    inital setup

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '@/components/authentication/LoginForm'
-
-export default function LoginPage() {
-  return (
-    <div className='grid  items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <LoginForm/>
-    </div>
-  )
-}
