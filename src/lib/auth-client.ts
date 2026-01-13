22ab10e805a7ddde653303c4c0ed60e3bbc9b79c
deveshcse <d.mishra56603@gmail.com>
2026-01-13 21:48:15 +0530

    feat: Add Event model to schema and enhance auth client with admin plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,11 @@
 import { createAuthClient } from "better-auth/react"
-import { inferAdditionalFields } from "better-auth/client/plugins";
+import { inferAdditionalFields, adminClient } from "better-auth/client/plugins";
 import { auth } from "./auth"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    plugins: [inferAdditionalFields<typeof auth>()],
+    plugins: [inferAdditionalFields<typeof auth>(), adminClient()],
 
 })
 export const { signIn, signUp, signOut, useSession } = authClient
