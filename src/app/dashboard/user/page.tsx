dce995e9e60636f43ce836a35cc37ac002d1b47d
deveshcse <d.mishra56603@gmail.com>
2026-01-24 22:29:53 +0530

    temp

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -5,15 +5,15 @@ import { redirect } from "next/navigation";
 
 
 export default async function Page() {
-    // const session = await auth.api.getSession({ headers: await headers() })
-  // if(session && session.user.role == "ADMIN"){
-  //   redirect("/dashboard/events")
-  // }
+    const session = await auth.api.getSession({ headers: await headers() })
+  if(session && session.user.role == "admin"){
+    redirect("/dashboard/events")
+  }
     return (
         <div className="mx-5 my-5">
             <h1>Participant Page</h1>
              <SignOutButton />
-            {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+            <pre>{JSON.stringify(session, null, 2)}</pre>
            
         </div>
     )
