af57ce3bdb663179ecacbd9c87097f4aa35b6615
deveshcse <d.mishra56603@gmail.com>
2026-01-25 12:22:47 +0530

    fix

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -4,20 +4,20 @@ import { defaultStatements, adminAc } from "better-auth/plugins/admin/access";
 
 const statement = {
     ...defaultStatements,
-  techfest: ["create", "update", "delete", "list", "read", "publish"],
+  techfest: ["create", "update", "delete", "read", "publish"],
 } as const;
 
 export const ac = createAccessControl(statement);
 
 export const user = ac.newRole({
-  techfest: ["list", "read"],
+  techfest: [ "read"],
 }) ;
 
 export const admin = ac.newRole({
-  techfest: ["create", "update", "delete", "list", "read", "publish"],
+  techfest: ["create", "update", "delete", "read", "publish"],
   ...adminAc.statements,
 });
 
 export const organizer = ac.newRole({
-  techfest: ["update", "list", "read"],
+  techfest: ["create", "update", "read"],
 });
