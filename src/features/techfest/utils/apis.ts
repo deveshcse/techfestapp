4cdae27a5d9c010702461462dc5205458efd1733
deveshcse <d.mishra56603@gmail.com>
2026-02-13 00:19:28 +0530

    feat: add update, delete, and toggle status functionalities for TechFest

--- src/features/techfest/utils/apis.ts
+++ src/features/techfest/utils/apis.ts
@@ -1,18 +1,42 @@
 import api from "@/lib/axios";
-import { CreateTechFestInput, TechFestDetails, TechFestListResponse } from "../types/techfest.types";
+import {
+  CreateTechFestInput,
+  TechFestDetails,
+  TechFestListResponse,
+} from "../types/techfest.types";
 
-export const listTechFest = async (): Promise<TechFestListResponse>  => {
+export const listTechFest = async (): Promise<TechFestListResponse> => {
   const response = await api.get("/api/techfest");
   return response.data;
 };
 
-export const createTechFest = async(formData: CreateTechFestInput) => {
+export const createTechFest = async (formData: CreateTechFestInput) => {
   const response = await api.post("/api/techfest", formData);
   return response.data;
-}
+};
 
-export const getTechfestDetails = async (id: number): Promise<TechFestDetails> => {
+export const getTechfestDetails = async (
+  id: number,
+): Promise<TechFestDetails> => {
   console.log("Fetching details for TechFest ID:", id);
   const response = await api.get(`/api/techfest/${id}`);
   return response.data;
-}
+};
+
+export const deleteTechFest = async (id: number) => {
+  const response = await api.delete(`/api/techfest/${id}`);
+  return response.data;
+};
+
+export const updateTechFest = async (
+  id: number,
+  formData: CreateTechFestInput,
+) => {
+  const response = await api.put(`/api/techfest/${id}`, formData);
+  return response.data;
+};
+
+export const toggleTechFestStatus = async (id: number) => {
+  const response = await api.patch(`/api/techfest/${id}`);
+  return response.data;
+};
--- src/features/techfest/utils/useTechFest.ts
+++ src/features/techfest/utils/useTechFest.ts
@@ -1,6 +1,17 @@
 import { keepPreviousData, useMutation, useQuery } from "@tanstack/react-query";
-import { createTechFest, getTechfestDetails, listTechFest } from "./apis";
-import { TechFestDetails, TechFestListResponse } from "../types/techfest.types";
+import {
+  createTechFest,
+  deleteTechFest,
+  getTechfestDetails,
+  listTechFest,
+  toggleTechFestStatus,
+  updateTechFest,
+} from "./apis";
+import {
+  CreateTechFestInput,
+  TechFestDetails,
+  TechFestListResponse,
+} from "../types/techfest.types";
 import { queryClient } from "@/lib/query-client";
 import { toast } from "sonner";
 
@@ -22,12 +33,42 @@ export function useCreateTechFest() {
   });
 }
 
-
 export function useTechFestDetails(id: number) {
   return useQuery<TechFestDetails>({
     queryKey: ["techfest", id],
     queryFn: () => getTechfestDetails(id),
     placeholderData: keepPreviousData,
-    
+  });
+}
+
+// add update, delete and publish/unpublish mutations here
+
+export function useUpdateTechFest(id: number) {
+  return useMutation({
+    mutationFn: (formData: CreateTechFestInput) => updateTechFest(id, formData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["techfest"] });
+      toast.success("TechFest updated successfully");
+    },
+  });
+}
+
+export function useToggleTechFestStatus(id: number) {
+  return useMutation({
+    mutationFn: () => toggleTechFestStatus(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["techfest"] });
+      toast.success("TechFest status updated successfully");
+    },
+  });
+}
+
+export function useDeleteTechFest(id: number) {
+  return useMutation({
+    mutationFn: () => deleteTechFest(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["techfest"] });
+      toast.success("TechFest deleted successfully");
+    },
   });
 }
